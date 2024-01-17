0613fe60eb0f6382dc9ee15df44f3ce4ec7dc7d7
Nahjay <nahjaybattieste@gmail.com>
2024-01-16 22:37:30 -0500

    wip: adding in some error checking

--- user_app/user_app.c
+++ user_app/user_app.c
@@ -14,17 +14,67 @@
 #define DEVICE_1 "/dev/kernel_module"
 #define DEVICE_2 "/dev/kernel_module_2"
 #define MAX_PATH 256
+#define IOCTL_CMD_PROCESS_IMAGE _IOWR('k', 1, char *)
 
-// Open device
-int open_device(char *device) {
-    int fd = open(device, O_RDWR);
+
+// Function to open kernel module
+int open_kernel_module(char *device) {
+    int fd;
+    fd = open(device, O_RDWR);
     if (fd < 0) {
-        fprintf(stderr, "Could not open device %s\n", device);
+        perror("Failed to open device");
         exit(EXIT_FAILURE);
     }
-    return fd;
+    else {
+        printf("Successfully opened device\n");
+        return fd;
+    }
 }
 
+// Function to close kernel module
+int close_kernel_module(int fd) {
+    close(fd);
+
+    // Check if close was successful
+    if (fd < 0) {
+        perror("Failed to close device");
+        exit(EXIT_FAILURE);
+    }
+    else {
+        printf("Successfully closed device\n");
+        return 0;
+    }
+}
+
+// Function to process image
+void process_image(int *fd, const char* input_path) {
+
+    // Allocate memory for image_path and cast to char*
+    char* image_path = (char*) malloc(MAX_PATH * sizeof(char));
+
+    
+    // Check if input_path is valid
+    if (input_path == NULL) {
+        fprintf(stderr, "Please input a valid path\n");
+        exit(EXIT_FAILURE);
+
+        // Free memory
+        free(image_path);
+
+        // Set pointer to heap memory to NULL
+        image_path = NULL;
+    }
+    else {
+        printf("Input path is valid\n");
+
+        // Check if input_path is a valid iamge file
+
+        
+
+
+    }
+
+}
 
 int main (int argc, char *argv[]) {
 
