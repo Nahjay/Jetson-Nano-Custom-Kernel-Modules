8bd8960cc7759f5f66923d2a5c64b5cbc1d8f1e1
Nahjay <nahjaybattieste@gmail.com>
2024-01-16 22:05:49 -0500

    wip: added beginning prompt to test user_app

--- user_app/user_app.c
+++ user_app/user_app.c
@@ -14,8 +14,19 @@
 
 
 
+int main (int argc, char *argv[]) {
 
 
+    // Quick error checking to make sure user app is being used correctly
+    printf("User app started\n");
+    printf("You have inputed %d arguments\n", argc);
+
+    if (argc != 2) {
+        printf("Please only input one file name\n");
+        return -1;
+    }
+    
+}
 
 
 
