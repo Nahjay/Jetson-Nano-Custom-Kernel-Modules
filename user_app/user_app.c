4a611ee6c091adf01b029c0b137a00a4ac7d7679
Nahjay <nahjaybattieste@gmail.com>
2024-01-16 22:23:24 -0500

    feat: added headerfile

--- user_app/user_app.c
+++ user_app/user_app.c
@@ -7,24 +7,45 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
+#include "user_app.h"
+
 
 
 #define DEVICE_1 "/dev/kernel_module"
-#define DEVICE_2 "/dev/kernel_module2"
+#define DEVICE_2 "/dev/kernel_module_2"
+#define MAX_PATH 256
 
+// Open device
+int open_device(char *device) {
+    int fd = open(device, O_RDWR);
+    if (fd < 0) {
+        fprintf(stderr, "Could not open device %s\n", device);
+        exit(EXIT_FAILURE);
+    }
+    return fd;
+}
 
 
 int main (int argc, char *argv[]) {
-
 
     // Quick error checking to make sure user app is being used correctly
     printf("User app started\n");
     printf("You have inputed %d arguments\n", argc);
 
     if (argc != 2) {
-        printf("Please only input one file name\n");
-        return -1;
+        fprintf(stderr, "Please only input one file name. Usage: %s <image_path>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
+    else {
+        printf("You have inputed '%s' as the image path\n", argv[1]);
+    }
+
+
+
+
+
+
+
     
 }
 
---
+++ user_app/user_app.h
@@ -0,0 +1,10 @@
+#ifndef USER_APP_H
+#define USER_APP_H
+
+#define MAX_PATH 256
+
+int open_kernel_module(const char* device_path);
+int close_kernel_module(int fd);
+void process_image(int fd, const char* input_path);
+
+#endif // USER_APP_H
