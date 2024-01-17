0e77b972c11fe8beb63644d7c7946c8ac2575897
Nahjay <nahjaybattieste@gmail.com>
2024-01-16 21:43:47 -0500

    fix: fixed gitignore for the user_app

---
+++ user_app/user_app.c
@@ -0,0 +1,15 @@
+// Create User App to interact with the kernel module
+// User app will pass in an image file to the kernel module
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+
+
+
+
+
