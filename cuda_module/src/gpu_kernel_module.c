b6d1cf40ee8308554251f5cbae8e8f291762af46
Nahjay <nahjaybattieste@gmail.com>
2024-01-25 11:21:59 -0500

    feat: beggining ioctl implementation

--- cuda_module/src/gpu_kernel_module.c
+++ cuda_module/src/gpu_kernel_module.c
@@ -17,5 +17,17 @@ MODULE_DESCRIPTION("Kernel module for changing the colors of a PPM image using G
 
 #define IMAGE_WIDTH 1280
 #define IMAGE_HEIGHT 853
+#define MAX_PATH 256
 #define IMAGE_SIZE (IMAGE_WIDTH * IMAGE_HEIGHT * 3) // Assuming RGB format
+
+// CUDA kernel
+extern void cuda_kernel(unsigned char *data, size_t width, size_t height, size_t metadata_size);
+
+// Create ioctl_handler
+static long ioctl_handler(struct file *file, unsigned int cmd, unsigned long arg) {
+
+
+    // Copy image from user space to kernel space
+
+}
 
