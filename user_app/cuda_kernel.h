9bd44424145298c88d0ae69073e971cead7ab940
Nahjay <nahjaybattieste@gmail.com>
2024-01-28 22:53:28 -0500

    chore: massive cleanup

--- user_app/cuda_kernel.h
+++
@@ -1,8 +0,0 @@
-// cuda_kernel.h
-
-#ifndef CUDA_KERNEL_H
-#define CUDA_KERNEL_H
-
-int process_image_data(char *argv[]);
-
-#endif /* CUDA_KERNEL_H */
--- user_app/user_app.cu
+++ user_app/user_app.cu
@@ -8,7 +8,6 @@
 #include "user_app.h"
 #include <dlfcn.h>
 #include "/usr/local/cuda-10.2/include/cuda_runtime.h"
-#include "cuda_kernel.h"
 #include "/usr/local/cuda-10.2/include/cuda.h"
 #include "/usr/local/cuda-10.2/include/device_launch_parameters.h"
 
