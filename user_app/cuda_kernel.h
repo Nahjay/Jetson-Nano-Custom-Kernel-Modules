7cc8c385bfd2ff4aa4707a975cbef770bb26fcd1
Nahjay <nahjaybattieste@gmail.com>
2024-01-28 19:56:35 -0500

    chore: more refactoring with includes

--- user_app/cuda_kernel.h
+++ user_app/cuda_kernel.h
@@ -3,14 +3,8 @@
 #ifndef CUDA_KERNEL_H
 #define CUDA_KERNEL_H
 
-#ifdef __cplusplus
 extern "C" {
-#endif
-
 void process_image_data(char *image_data);
-
-#ifdef __cplusplus
 }
-#endif
 
 #endif /* CUDA_KERNEL_H */
