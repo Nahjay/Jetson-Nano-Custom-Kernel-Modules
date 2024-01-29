32af6f378234b2333d926268cf8b0ec393a32d0c
Nahjay <nahjaybattieste@gmail.com>
2024-01-28 19:21:17 -0500

    fix: added __global__ to header file

--- user_app/cuda_kernel.h
+++ user_app/cuda_kernel.h
@@ -3,6 +3,6 @@
 #ifndef CUDA_KERNEL_H
 #define CUDA_KERNEL_H
 
-void cuda_kernel(unsigned char *image_data, size_t width, size_t height, size_t metadata_size);
+__global__ void cuda_kernel(unsigned char *image_data, size_t width, size_t height, size_t metadata_size);
 
 #endif /* CUDA_KERNEL_H */
